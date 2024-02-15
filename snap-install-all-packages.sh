7c0bd69ac4a91f4dd88758fb6fc1eefc65c07be0
Raheny Roger <heniroger@yahoo.fr>
2024-02-15 14:52:03 +0000

    Update snap-install-all-packages.sh

--- snap-install-all-packages.sh
+++ snap-install-all-packages.sh
@@ -21,3 +21,107 @@ sudo snap install drawio &&\
 sudo snap install thunderbird &&\
 sudo snap install dbeaver-ce &&\
 sudo snap install gimp
+sudo snap install umlet-standalone
+sudo snap install terraform --classic
+sudo snap install leafpad
+sudo snap install jenkins --classic
+sudo snap install sqlitebrowser
+sudo snap install mysql-workbench-community
+sudo snap install nvim --classic
+sudo snap install astronum
+sudo snap install gelectrical
+sudo snap install simufisica
+sudo snap install freecad-mechanical
+sudo snap install gnuplot-editor-unofficial
+sudo snap install logarithmplotter
+sudo snap install ubpm
+sudo snap install freemat-unofficial
+sudo snap install mapollage
+sudo snap install openrocket
+sudo snap install pyresistors
+sudo snap install jupyter
+sudo snap install octave
+sudo snap install easy-file-organizer
+sudo snap install qsnapstore
+sudo snap install arduino
+sudo snap install steam
+sudo snap install brave
+sudo snap install telemetrytv
+sudo snap install chromium
+sudo snap install rapidsvn-snap
+sudo snap install qbittorrent-arnatious
+sudo snap install monophony
+sudo snap install djpdf
+sudo snap install pratical-alarm
+sudo snap install youtube-downloader
+sudo snap install mkdocs-carroarmato0
+sudo snap install xdman --edge
+sudo snap install vision9-access-point --edge
+sudo snap install chatgpt-desktop
+sudo snap install transmission
+sudo snap install uget --edge
+sudo snap install atril
+sudo snap install audio-looper
+sudo snap install upnp-router-control
+sudo snap install video-downloader
+sudo snap install ssmtp-snap
+sudo snap install ftp-manager
+sudo snap install firefox
+sudo snap install opera
+sudo snap install blue-recorder
+sudo snap install mumble
+sudo snap install audacity
+sudo snap install ffmpeg
+sudo snap install nordpass
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
