3fad4b6ef5533f315649e6972c5c458ec1eaa847
Raheny Roger <heniroger@yahoo.fr>
2024-02-15 14:04:28 +0000

    Update snap-install-all-packages.sh

--- snap-install-all-packages.sh
+++ snap-install-all-packages.sh
@@ -1,1 +1,23 @@
-.
+#!/bin/bash
+
+sudo apt-get install -y snapd &&\
+sudo snap install snap-store &&\
+sudo snap install obs-studio &&\
+sudo snap install easy-disk-cleaner &&\
+sudo snap install freecad &&\
+sudo snap install blender --classic &&\ 
+sudo snap install inkscape &&\
+sudo snap install youtube-dl &&\
+sudo snap install ultimate-media-downloader &&\
+sudo snap install clementine &&\
+sudo snap install ngrok &&\
+sudo snap install vlc &&\
+sudo snap install mailspring &&\
+sudo snap install discord &&\
+sudo snap install skype &&\
+sudo snap install code --classic &&\
+sudo snap install postman &&\
+sudo snap install drawio &&\
+sudo snap install thunderbird &&\
+sudo snap install dbeaver-ce &&\
+sudo snap install gimp
